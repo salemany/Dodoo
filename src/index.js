9eda89006092c5c27fb9d2b52b1bbaaca7d6093e
Sebastian Alemany <sebastianalemany22@gmail.com>
2022-03-22 16:17:16 -0300

    Initials
    Initials Code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const rutas = require('./routes/index');
+
+app.set("port", process.env.PORT || 3000);
+app.use('/', rutas);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get("port"), () => {
+    console.log("Server Run on port: " + app.get("port"));
+});
