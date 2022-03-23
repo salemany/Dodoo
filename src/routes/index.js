1b31bf740fe196c3ea3f2d0ef9b2149744c2813a
Sebastian Alemany <33409480+salemany@users.noreply.github.com>
2022-03-22 22:34:54 -0300

    languaje index and content

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {FileCSS: './css/main.css'});
+})
+
+router.get('/de/', (req, res) => {
+    res.render('deutch', {FileCSS: '../css/main.css'});
+})
+
+router.get('/sp/', (req, res) => {
+    res.render('spanish', {FileCSS: '../css/main.css'});
 })
 
 module.exports = router;
