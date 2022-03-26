758035cbcff3b05c48d35a511cabc02ae4484a0d
Sebastian Alemany <33409480+salemany@users.noreply.github.com>
2022-03-26 18:34:54 -0300

    Favicon and CSS

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {FileCSS: './css/main.css'});
+    res.render('index', {FileCSS: './css/main.css', FileICO: './image/dodo.png'});
 })
 
 router.get('/de/', (req, res) => {
-    res.render('deutch', {FileCSS: '../css/main.css'});
+    res.render('deutch', {FileCSS: '../css/main.css', FileICO: '../image/dodo.png'});
 })
 
 router.get('/sp/', (req, res) => {
-    res.render('spanish', {FileCSS: '../css/main.css'});
+    res.render('spanish', {FileCSS: '../css/main.css', FileICO: '../image/dodo.png'});
 })
 
 module.exports = router;
